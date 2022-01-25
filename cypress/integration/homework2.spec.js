2778baf90d5297cb9df4dd0e383724565b29859d
NazarNech <98355504+NazarNech@users.noreply.github.com>
2022-01-25 12:14:11 +0200

    Update homework2.spec.js

--- cypress/integration/homework2.spec.js
+++ cypress/integration/homework2.spec.js
@@ -1,6 +1,6 @@
 describe('Homework2', () => {
 
-    it('Open the site', () => {
+    it('Verifying the text', () => {
       cy.visit('https://allo.ua/ru/');
 
       cy.get('a.mh-button').eq(0).click();
