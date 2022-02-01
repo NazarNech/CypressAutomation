2fda55021cd24c3c5f239cb5043aed40de4d8b9a
NazarNech <98355504+NazarNech@users.noreply.github.com>
2022-02-01 12:11:50 +0200

    Add files via upload

---
+++ cypress/integration/homework3.spec.js
@@ -0,0 +1,18 @@
+describe('Homework3', () => {
+
+    it('Verify filter', () => {
+      cy.visit('https://allo.ua/ru/');
+
+      cy.get('[id="search-form__input"]').type("xiaomi");
+      cy.get('.search-form__submit-button').click();
+
+      cy.get('div[data-product-id]')
+        .should('have.length', 28); 
+
+      cy.get('[id="filter_cat_id-48"]').click();
+      cy.get('.filter-popup__button').click()
+
+      cy.get('a.active-filter')
+        .should('include.text', 'Смартфоны и мобильные телефоны')
+    })
+  })
