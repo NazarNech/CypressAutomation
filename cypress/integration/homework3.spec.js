a0b6c3ba7f1135f0efff0a9237cb2bc59a850be8
NazarNech <98355504+NazarNech@users.noreply.github.com>
2022-02-03 09:32:13 +0200

    Update homework3.spec.js

--- cypress/integration/homework3.spec.js
+++ cypress/integration/homework3.spec.js
@@ -3,16 +3,16 @@ describe('Homework3', () => {
     it('Verify filter', () => {
       cy.visit('https://allo.ua/ru/');
 
-      cy.get('[id="search-form__input"]').type("xiaomi");
+      cy.get('[id=search-form__input]').type('xiaomi');
       cy.get('.search-form__submit-button').click();
 
       cy.get('div[data-product-id]')
-        .should('have.length', 28); 
+            .should('have.length', 28); 
 
-      cy.get('[id="filter_cat_id-48"]').click();
-      cy.get('.filter-popup__button').click()
+      cy.get('[id=filter_cat_id-48]').click();
+      cy.get('.filter-popup__button').click();
 
       cy.get('a.active-filter')
-        .should('include.text', 'Смартфоны и мобильные телефоны')
-    })
-  })
+            .should('include.text', 'Смартфоны и мобильные телефоны');
+    });
+  });
